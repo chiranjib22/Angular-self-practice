ec02304189a199f574a94797e935dce2808e5861
chiranjib22 <chiranjib.chakraborty.cse@gmail.com>
2026-06-28 01:12:40 +0600

    ok

--- book-app/src/app/services/book.service.ts
+++ book-app/src/app/services/book.service.ts
@@ -2,23 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Book } from '../book-list/book.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class BookService {
+
+  baseUrl: string = environment.baseUrl;
   constructor(private http: HttpClient) {}
 
   getAllBooks(): Observable<Book[]> {
-    return this.http.get<Book[]>('/api/books');
+    return this.http.get<Book[]>(`${this.baseUrl}/api/books`);
   }
 
   getBook(id: number): Observable<Book> {
-    return this.http.get<Book>(`/api/books/${id}`);
+    return this.http.get<Book>(`${this.baseUrl}/api/books/${id}`);
   }
 
   save(book: Book): Observable<Book> {
     if (!book.id) {
-      return this.http.post<Book>('/api/books', book);
+      return this.http.post<Book>(`${this.baseUrl}/api/books`, book);
     }
-    return this.http.put<Book>(`/api/books/${book.id}`, book);
+    return this.http.put<Book>(`${this.baseUrl}/api/books/${book.id}`, book);
   }
 }
